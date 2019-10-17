17f098b8753b0d2b2b45f37f13db49ac4aeeb269
Mohit <mohitsahni1234@gmail.com>
2019-10-17 15:29:47 +0530

    committing 2

---
+++ email/email-db/src/main/java/org/email/db/App.java
@@ -0,0 +1,13 @@
+package org.email.db;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
